b36c0d85cc0ee242398f29b27458f34c382e3122
inoks138 <andriipankov138@gmail.com>
2022-02-09 13:45:37 +0200

    Added models to the shop app

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,8 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mptt',
 
-    'shop.apps.ShopConfig'
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -94,3 +95,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from slugify import slugify
 
-# Create your models here.
+
+class Category(MPTTModel):
+    title = models.CharField(max_length=50, verbose_name="Название")
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    slug = models.SlugField(max_length=60, unique=True, verbose_name="url", null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_products(self):
+        """Return all self and descendants products"""
+        categories = self.get_descendants(include_self=True)
+        products = Product.objects.filter(category__in=categories)
+        return products
+
+    def save(self, **kwargs):
+        super(Category, self).save()
+        if not self.is_root_node():
+            root = self.get_root()
+            if not self.slug.startswith(root.slug):
+                self.slug = root.slug + '-' + slugify(self.title)
+                super(Category, self).save()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    class MPTTMeta:
+        order_insertion_by = ['-title']
+
+
+class Brand(models.Model):
+    title = models.CharField(max_length=30, unique=True, verbose_name="Название")
+    slug = models.SlugField(max_length=30, unique=True, verbose_name="url")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+        ordering = ['title']
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название")
+    slug = models.SlugField(max_length=111, verbose_name="url", unique=True)
+    photo = models.ImageField(upload_to='products/%Y/%m/%d/', verbose_name="Фото")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    stock = models.PositiveIntegerField(verbose_name="Количество")
+    is_available = models.BooleanField(default=True, verbose_name="Доступен")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Изменено")
+    category = TreeForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория')
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, verbose_name='Бренд')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, **kwargs):
+        super(Product, self).save()
+        if not self.slug.endswith('-' + str(self.pk)):
+            self.slug += '-' + str(self.id)
+            super(Product, self).save()
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['-created_at']
